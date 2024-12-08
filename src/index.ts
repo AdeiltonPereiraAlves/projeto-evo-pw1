e3a2d5a3f084e7172c1b99961dad2420f9e241a7
adeilton <adeutoo@gmail.com>
2024-12-07 21:42:17 -0300

    iniciando projeto pw1

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const a = 1; 
+const b = 3;
+console.log(a + b)
+export default function soma(a:number, b: number){
+    return a + b
+}
---
+++ test/calc.test.ts
@@ -0,0 +1,5 @@
+import soma from "../src/index"
+
+test('deve somar dois numeros', () => {
+   expect(soma(2,2)).toBe(4)
+})
