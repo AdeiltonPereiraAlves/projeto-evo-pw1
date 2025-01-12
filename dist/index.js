84e40a2191956a890a92fc959606083910ff9ae8
adeilton <adeutoo@gmail.com>
2025-01-12 19:33:13 -0300

    Removendo pasta dist do monitoramento do Git

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = soma;
-var a = 1;
-var b = 3;
-console.log(a + b);
-function soma(a, b) {
-    return a + b;
-}
